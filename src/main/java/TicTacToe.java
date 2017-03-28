6ed6953c7c8c8a8f630ba6342e102c5ad51527d3
TamerlaN0210 <timurahmadulin@mail.ru>
2017-03-28 16:59:28 +0500

    init

---
+++ src/main/java/TicTacToe.java
@@ -0,0 +1,12 @@
+import gui.MainWindow;import java.lang.String;
+
+/**
+ * Main.
+ */
+
+public class TicTacToe {
+	public static void main(String args[]) {
+		MainWindow mainWindow = new MainWindow();
+		mainWindow.setVisible(true);
+	}
+}
